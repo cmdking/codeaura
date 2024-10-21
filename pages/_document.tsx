f6ab36ce3f17b136a54d796bff146f48ff87dd1e
cmdking <your-email@example.com>
2024-10-21 23:17:32 +0200

    Added gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,20 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VG171MHLKJ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-VG171MHLKJ');
+            `,
+          }}
+        />
+        {/* Your other head tags */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Lemon&display=swap" rel="stylesheet" />
